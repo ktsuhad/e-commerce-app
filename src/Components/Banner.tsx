f8c22f9117fa7945f471e920bc03234aa54367ce
SUHAD.KT <suhadkizhakethodi@gmail.com>
2023-08-12 22:42:15 +0530

    Banner added

---
+++ src/Components/Banner.tsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+      <div className='bg-[#FAF0D7] text-[#125C13] flex items-center  rounded-md'>
+          <div className='flex-1 px-4 md:px-24'>
+              <p className='text-base md:text-3xl font-medium '>Grab Upto 50% Off On<br/>Selected Headphone</p>
+              <button className='px-4 py-1 bg-[#125C13] text-white rounded-full mt-3'>Buy Now</button>
+          </div>
+          <div className='flex-1'>
+              <img src='Assets/CarouselImage.png' alt="" className='w-40 h-52 object-cover'/>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Banner
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
+import Banner from "../Components/Banner";
 import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <div className="container mx-auto px-3">
       <Navbar />
+      <Banner/>
     </div>
   );
 };
