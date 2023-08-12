bec32d0d7a7961dd7a57e899b4c57530b5aa999f
SUHAD.KT <suhadkizhakethodi@gmail.com>
2023-08-12 23:33:26 +0530

    product added

---
+++ src/Components/ProductCard.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import React from "react";
+import { FavoriteBorderOutlined } from "@mui/icons-material";
+import { Product } from "../Types/types";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="w-64">
+      <Link
+        to={`/productdetails/${product.id}`}
+        className="bg-slate-100 w-full h-56 flex items-center justify-center rounded-md relative"
+      >
+        <img src={product.images[0]} alt="" className="w-32 h-auto" />
+        <span className="text-sm absolute top-2 right-2 bg-white rounded-full hover:bg-teal-500 hover:text-white flex px-2 py-1.5">
+          <FavoriteBorderOutlined />
+        </span>
+      </Link>
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-between">
+          <h1 className="font-bold">{product.title}</h1>
+          <span className="font-bold">$ {product.price}</span>
+        </div>
+        <p className="text-ellipsis whitespace-nowrap overflow-hidden text-sm text-gray-500">
+          {product.description}
+        </p>
+        {/* <StarRating /> */}
+        <div className="flex justify-center">
+          <Button
+            variant="contained"
+            fullWidth
+            size="small"
+            style={{ backgroundColor: "green", marginTop: "5px" }}
+          >
+            Add to Cart
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,16 @@
-import Banner from "../Components/Banner";
-import Navbar from "../Components/Navbar";
+import Banner from "../Components/Banner";  //Banner
+import Navbar from "../Components/Navbar";  //Navbar
+import ProductListing from "../Components/ProductListing";
+import productData from "../utils/Data.json";
 
 const Home = () => {
+console.log(productData);
+
   return (
     <div className="container mx-auto px-3">
       <Navbar />
       <Banner/>
+      <ProductListing/>
     </div>
   );
 };
